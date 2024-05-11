d82d7551f07ffb19ce7490da26547e27707fb977
Daniel Cavalheiro <danielmilsomcavalheiro@gmail.com>
2024-05-11 16:07:03 +0100

    Removed line num from AbsToken

--- utils/abstract_token.py
+++ utils/abstract_token.py
@@ -1,16 +1,18 @@
-class AbsToken:
-    """Abstract token class to represent tokens after abstracting."""
+"""Abstract token class to represent tokens after abstraction and correlation."""
 
-    def __init__(self, token_type, line_num, lexpos, depth, order, flow_type):
+
+class AbsToken:
+    """Abstract token class to represent tokens after abstraction and correlation."""
+
+    def __init__(self, token_type, lexpos, depth, order, flow_type):
         self.token_type = token_type
-        self.line_num = line_num
         self.lexer_pos = lexpos
         self.depth = depth
         self.order = order
         self.flow_type = flow_type
 
     def __str__(self):
-        return f"AbsToken({self.token_type}, {self.line_num}, {self.lexer_pos}, {self.depth}, {self.order}, {self.flow_type})"
+        return f"AbsToken({self.token_type}, {self.lexer_pos}, {self.depth}, {self.order}, {self.flow_type})"
 
     def __eq__(self, value: object) -> bool:
         return self.__str__() == value.__str__()
