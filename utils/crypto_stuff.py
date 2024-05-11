f80208e8bc1f79372e7bbeee6911e660f336f41a
Daniel Cavalheiro <danielmilsomcavalheiro@gmail.com>
2024-05-11 18:11:00 +0100

    Added crypto utility functions for sse, ope, hashing, and HMAC

---
+++ utils/crypto_stuff.py
@@ -0,0 +1,56 @@
+"""This module contains functions for encrypting and hashing data."""
+
+import hashlib
+import hmac
+from Crypto.Cipher import AES
+from pyope.ope import OPE
+
+
+# ------------------------------------ SSE ----------------------------------- #
+
+def generate_key(password):
+    """Generates a 256-bit key from a password."""
+    hasher = hashlib.sha256()
+    hasher.update(password.encode())
+    return hasher.digest()
+
+
+def encrypt_sse(data, password):
+    """Encrypts data using deterministic AES in SIV mode."""
+    key = generate_key(password)
+    cipher = AES.new(key, AES.MODE_SIV)
+    ciphertext, tag = cipher.encrypt_and_digest(data.encode())
+    return ciphertext + tag
+
+
+# Here for testing purposes and will not be used in the final implementation
+def decrypt_sse(encrypted_data, password):
+    """Decrypts data using deterministic AES in SIV mode."""
+    key = generate_key(password)
+    cipher = AES.new(key, AES.MODE_SIV)
+    ciphertext, tag = encrypted_data[:-16], encrypted_data[-16:]
+    decrypted_data = cipher.decrypt_and_verify(ciphertext, tag)
+    return decrypted_data.decode()
+
+
+# ------------------------------------ OPE ----------------------------------- #
+
+def encrypt_ope(data, password):
+    """Encrypts data using Order Preserving Encryption."""
+    ope = OPE(password)
+    return ope.encrypt(int(data))
+
+
+# ----------------------------------- HASH ----------------------------------- #
+
+def hash_it(data):
+    """Hashes data using SHA-256."""
+    h = hashlib.sha256()
+    h.update(data.encode())
+    return h.hexdigest()
+
+
+def hmac_it(data, password):
+    """Hashes data using HMAC with SHA-256."""
+    h = hmac.new(password.encode(), data.encode(), hashlib.sha256)
+    return h.hexdigest()
